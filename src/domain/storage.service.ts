43cdf275c269a18411b391f80679e0ebe5d099c6
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-10 23:57:31 +0500

    init commit

--- src/domain/storage.service.ts
+++ src/domain/storage.service.ts
@@ -1,4 +1,6 @@
 import { homedir } from 'os'
+import { join } from 'path'
+
 export const saveKeyValue = (key: string, value: any) => {
-    console.log(homedir())
+    console.log(join(homedir(), '../weather-data.json'));
 }
