5fbc631376900206ed32bc1a614f8edc80ecb962
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-10 23:40:16 +0500

    init commit

--- src/domain/log.service.ts
+++ src/domain/log.service.ts
@@ -1,3 +1,20 @@
-const printError = (error: any) => {
-    console.log('ERROR ' + error)
+import bgRed from 'chalk'
+import bgGreen from 'chalk'
+import bgCyan from 'chalk'
+
+export const printError = (error: string) => {
+    console.log(bgRed(' ERROR ') + ' ' + error)
+}
+
+export const printSuccess = (msg: string) => {
+    console.log(bgGreen(' SUCCESS ') + ' ' + msg)
+}
+
+export const printHelp = () => {
+    console.log(
+        `${bgCyan(' HELP ')} 
+        Без параметров - вывод погоды
+        -s [CITY] для установки города
+        -h для вывода помощи
+        -t [API_KEY] для сохранения токена`)
 }
