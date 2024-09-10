04b1d247397ad3ee981f497bd12d5142a66b3d34
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-10 23:41:39 +0500

    init commit

--- src/domain/log.service.ts
+++ src/domain/log.service.ts
@@ -1,18 +1,16 @@
-import bgRed from 'chalk'
-import bgGreen from 'chalk'
-import bgCyan from 'chalk'
+import chalk from 'chalk'
 
 export const printError = (error: string) => {
-    console.log(bgRed(' ERROR ') + ' ' + error)
+    console.log(chalk.bgRed(' ERROR ') + ' ' + error)
 }
 
 export const printSuccess = (msg: string) => {
-    console.log(bgGreen(' SUCCESS ') + ' ' + msg)
+    console.log(chalk.bgGreen(' SUCCESS ') + ' ' + msg)
 }
 
 export const printHelp = () => {
     console.log(
-        `${bgCyan(' HELP ')} 
+        `${chalk.bgCyan(' HELP ')} 
         Без параметров - вывод погоды
         -s [CITY] для установки города
         -h для вывода помощи
