d2c685c2a96519075ed7f948c57d029972af97ce
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 00:00:17 +0500

    init commit

--- src/domain/storage.service.ts
+++ src/domain/storage.service.ts
@@ -1,5 +1,5 @@
 import { homedir } from 'os'
-import { join, basename, dirname, extname } from 'path'
+import { join, basename, dirname, extname, relative } from 'path'
 
 export const filePath = join(homedir(), '../weather-data.json');
 
@@ -7,4 +7,5 @@ export const saveKeyValue = (key: string, value: any) => {
     console.log(basename(filePath));
     console.log(dirname(filePath));
     console.log(extname(filePath));
+    console.log(relative(filePath, dirname(filePath)));
 }
