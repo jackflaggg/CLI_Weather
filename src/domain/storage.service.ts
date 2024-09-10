469642e27c231d918190de3948cba7e26021377c
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 00:23:52 +0500

    init commit

--- src/domain/storage.service.ts
+++ src/domain/storage.service.ts
@@ -15,6 +15,14 @@ export const saveKeyValue = async (key: string, value: any) => {
     await promises.writeFile(filePath, JSON.stringify(data))
 }
 
+export const getKeyValue = async (key: string) => {
+    if (await isExist(filePath)){
+        const file = await promises.readFile(filePath, 'utf8')
+        const data = JSON.parse(file);
+        return data[key]
+    }
+}
+
 export const isExist = async (path: string) => {
     try {
         await promises.stat(path);
