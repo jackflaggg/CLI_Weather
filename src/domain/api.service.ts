678a34a60691cf07f2d53dc7d33f6ed090ef59f4
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 01:07:51 +0500

    init commit

--- src/domain/api.service.ts
+++ src/domain/api.service.ts
@@ -5,10 +5,29 @@ import {getKeyValue, TOKEN_DICTIONARY} from "./storage.service.js";
 const getWeather = async (city: string) => {
     const token = await getKeyValue(TOKEN_DICTIONARY.token);
 
+    if(!token){
+        throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
+    }
     const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
 
     url.searchParams.append('q', city);
-    url.searchParams.append('appid', city);
+    url.searchParams.append('appid', token);
+    url.searchParams.append('lang', 'ru');
+    url.searchParams.append('units', 'metric');
 
-    https.get(url);
+    https.get(url, (response) => {
+        let res = '';
+
+        response.on('data', (chunk) => {
+            res += chunk
+        });
+
+        response.on('end', () => {
+            console.log(res);
+        });
+
+        response.on('error', (err) => {
+            console.log(res);
+        });
+    });
 };
