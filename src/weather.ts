9407af3b185a370538a1cc5a50a820d71187dfde
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-10 23:41:03 +0500

    init commit

--- src/weather.ts
+++ src/weather.ts
@@ -1,19 +1,20 @@
 import {getArgs} from "./helpers/args";
+import {printError, printHelp, printSuccess} from "./domain/log.service";
 
 const initCLI = (msg?: string) => {
     const args = getArgs(process.argv)
     console.log(args)
 
     if (args.h) {
-
+        printHelp();
     }
 
     if (args.s) {
-
+        printSuccess('suc');
     }
 
     if (args.t) {
-
+        printError('no')
     }
 };
 
