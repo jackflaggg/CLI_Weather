4244974ee44c57d41449dd50c866417ede62ffdf
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 00:41:04 +0500

    init commit

--- src/domain/api.service.ts
+++ src/domain/api.service.ts
@@ -1,3 +1,7 @@
-const getWeather = async (city: string) => {
+import https from 'https';
 
+const getWeather = async (city: string, key: string) => {
+    const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
+
+    https.get(url)
 };
