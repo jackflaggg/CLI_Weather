dc8bbea44c710dc1b79177ea5b225d187a3f9ca1
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 14:17:10 +0500

    init commit

--- src/domain/lang.service.ts
+++ src/domain/lang.service.ts
@@ -1,14 +1,11 @@
 import {printError, printSuccess} from "./log.service.js";
 import {saveKeyValue, TOKEN_DICTIONARY} from "./storage.service.js";
 
-let language = 'en';
-
-export const setLanguage = async (lang: string) => {
+export const setLanguage = async (lang: string = 'en') => {
     if (lang !== 'en' && lang !== 'ru') {
         printError('Поддерживаются только языки: en, ru');
         return;
     }
-    language = lang;
     await saveKeyValue(TOKEN_DICTIONARY.language, lang);
-    printSuccess(`Язык установлен на ${lang === 'en' ? 'английский' : 'русский'}`);
+    return printSuccess(`Язык установлен на ${lang === 'en' ? 'английский' : 'русский'}`);
 };
