529f226690055bd25ebc9092f8d0a5b3f9d7b2ed
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-11 14:15:35 +0500

    init commit

--- src/domain/api.service.ts
+++ src/domain/api.service.ts
@@ -1,37 +1,6 @@
 import {getKeyValue, TOKEN_DICTIONARY} from "./storage.service.js";
 import axios from "axios";
 
-export const getIcon = (icon: any) => {
-    let res = ''
-    switch (icon.slice(0, -1)) {
-        case '01':
-            res = '01';
-            break
-        case '02':
-            res = '02';
-            break
-        case '03':
-            res = '03';
-            break
-        case '04':
-            res = '04';
-            break
-        case '05':
-            res = '05';
-            break
-        case '06':
-            res = '06';
-            break
-        case '07':
-            res = '07';
-            break
-        case '08':
-            res = '09';
-            break
-    }
-    return res;
-}
-
 export const getWeather = async (city: string = 'moscow') => {
     const token = process.env.TOKEN ?? await getKeyValue(TOKEN_DICTIONARY.token);
 
