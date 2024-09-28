f3e8df9a4a8436e58656f53f6a9e9e77ba47cb5e
jackflaggg <rasul.khamzinnn@gmail.com>
2024-09-28 21:45:19 +0500

    init env

--- src/domain/api.service.ts
+++ src/domain/api.service.ts
@@ -1,22 +1,22 @@
 import {getKeyValue, tokenDictionary} from "./storage.service.js";
 import axios from "axios";
+import {SETTINGS} from "../settings.js";
 
 export const getWeather = async (city: string = 'moscow') => {
-    const token = process.env.TOKEN ?? await getKeyValue(tokenDictionary.token);
+    const token = SETTINGS.token ?? await getKeyValue(tokenDictionary.token);
 
     if(!token){
         throw new Error('Не задан ключ API, задайте его через команду -t [API_KEY]')
     }
 
-    const { data } = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    const { data } = await axios.get(SETTINGS.weatherURL, {
         params : {
             q: city,
             appid: token,
             lang: 'ru',
             units: 'metric'
         }
-    })
-    console.log(data)
+    });
 
     return data;
 
--- src/settings.ts
+++ src/settings.ts
@@ -2,5 +2,6 @@ import {config} from 'dotenv'
 config()
 
 export const SETTINGS = {
-
+    token: process.env.TOKEN || '',
+    weatherURL: process.env.OPENWEATHERURL || '',
 }
